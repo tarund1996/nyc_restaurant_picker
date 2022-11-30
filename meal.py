90cb34b8776c3cef7ae2e2e84b796ad111de553f
Tarun Dutt <tarund1996@gmail.com>
2022-11-30 11:38:27 -0500

    single rec

---
+++ meal.py
@@ -0,0 +1,7 @@
+from random import shuffle
+
+a = ['sweetgreen', 'bravest', 'hole in the wall', 'nyu langone cafe', 'sophra grill', 'marche madison', 'one two thai']
+
+shuffle(a)
+
+print('go here: ', a[0])
