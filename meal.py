96b205c02d570be31f6667fd3cb633ad040562bb
Tarun Dutt <tarund1996@gmail.com>
2023-03-10 18:22:16 -0500

    added norma

--- meal.py
+++ meal.py
@@ -1,6 +1,6 @@
 from random import shuffle
 
-a = ['sweetgreen', 'bravest', 'hole in the wall', 'nyu langone cafe', 'sophra grill', 'marche madison', 'one two thai']
+a = ['norma', 'sweetgreen', 'bravest', 'hole in the wall', 'nyu langone cafe', 'sophra grill', 'marche madison', 'one two thai', 'lan larb']
 
 shuffle(a)
 
